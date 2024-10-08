cdc9780f6e1de128b04f196920a1c1f21e2710b5
Joan <joan.corrals@autonoma.cat>
2024-10-08 18:16:26 +0200

    Añadir proyecto a github

---
+++ src/baseNoStates/DoorState.java
@@ -0,0 +1,13 @@
+package baseNoStates;
+
+public abstract class DoorState {
+  protected Door door;
+  protected String name;
+
+  public DoorState(Door door) { this.door = door; }
+
+  public abstract void open();
+  public abstract void close();
+  public abstract void lock();
+  public abstract void unlock();
+}
---
+++ src/baseNoStates/Unlocked.java
@@ -0,0 +1,26 @@
+package baseNoStates;
+
+public class Unlocked extends DoorState{
+
+  public Unlocked(Door door) { super(door); }
+
+  public void open()
+  {
+
+  }
+
+  public void close()
+  {
+
+  }
+
+  public void lock()
+  {
+
+  }
+
+  public void unlock()
+  {
+
+  }
+}
