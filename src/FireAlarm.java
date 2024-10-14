70b34d1a61b579c8c59d847688df914b4cca1f34
NIU1671253 <116256008+NIU1671253@users.noreply.github.com>
2024-10-14 17:18:29 +0200

    Update FireAlarm.java

--- src/FireAlarm.java
+++ src/FireAlarm.java
@@ -1,3 +1,25 @@
 public class FireAlarm extends CodeHandler {
 
+    private String codeFireAlarm;
+
+    public FireAlarm(String code, CodeHandler nextHandler) {
+        super(nextHandler);
+        codeFireAlarm = code;
+    }
+
+    @Override
+    public void handleCode(String code, Door door) {
+        System.out.println("handle FireAlarm");
+        if (!door.isLocked()) {
+            if (codeFireAlarm.equals(code)) {
+                door.resetState();
+                door.open();
+                System.out.println("NINOOOOO");
+            } else
+                super.handleCode(code, door);
+        }
+        else {
+            //void, can't open until unlocked
+        }
+    }
 }
