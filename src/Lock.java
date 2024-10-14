d7035a5a6e47a57e4b7552e34464bd1fff1f7b4d
NIU1671253 <116256008+NIU1671253@users.noreply.github.com>
2024-10-14 17:19:09 +0200

    Update Lock.java

--- src/Lock.java
+++ src/Lock.java
@@ -1,3 +1,17 @@
 public class Lock extends CodeHandler {
 
+    public Lock(CodeHandler nextHandler) {
+        super(nextHandler);
+    }
+    @Override
+    public void handleCode(String code, Door door) {
+        System.out.println("handle Lock");
+        if (!door.isLocked() && door.getNumTrials() >= 3) {
+            door.lock();
+        }
+        else {
+            //void, can't open until unlocked
+        }
+
+    }
 }
