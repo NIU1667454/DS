21c6765bd2e192dde1d0d87a5c36707fc3f57352
NIU1671253 <116256008+NIU1671253@users.noreply.github.com>
2024-10-14 17:20:01 +0200

    Update Unlock.java

--- src/Unlock.java
+++ src/Unlock.java
@@ -1,3 +1,22 @@
 public class Unlock extends CodeHandler {
 
+    private String codeUnlock;
+
+    public Unlock(String code, CodeHandler nextHandler) {
+        super(nextHandler);
+        codeUnlock = code;
+    }
+
+    @Override
+    public void handleCode(String code, Door door) {
+        System.out.println("handle Unlocked");
+        if (door.isLocked() && codeUnlock.equals(code)) {
+            door.resetState();
+        }
+        else {
+            //void, can't open until unlocked
+            super.handleCode(code, door);
+        }
+
+    }
 }
