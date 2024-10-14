77c0abb59ceeeb17ba57a334bb81057b20cd3059
NIU1671253 <116256008+NIU1671253@users.noreply.github.com>
2024-10-14 17:17:55 +0200

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,5 @@
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String openCode = "1111";
         String fireAlarmCode = "2222";
         String unlockCode = "3333";
@@ -9,29 +9,68 @@ public class Client {
                                 new Open(openCode,
                                         new Lock(null)))));
         CodeHandler chain2 = new Log(new Open(openCode, null));
-        CodeHandler chain3 =
-                new Log(
-                        new FireAlarm(code_fire_alarm,
-                                new Open(code_open, null)));
+
+        CodeHandler chain3 = new Log( new FireAlarm(fireAlarmCode, new Open(openCode, null)));
+
         Door d1 = new Door("d1", chain1);
+        System.out.println("Door d1 behaviour: Log - Unlock - FireAlarm - Open - Lock");
+        System.out.println("Open code: ");
+        d1.processCode("1111");
+        System.out.println("Open and fire the alarm: ");
+        d1.processCode("2222");
+        System.out.println("1st trial, wrong code: ");
+        d1.processCode("1234");
+        System.out.println("2nd trial, wrong code: ");
+        d1.processCode("4321");
+        System.out.println("3th trial, wrong code: ");
+        d1.processCode("5555");
+        System.out.println("Invalid Unlock code: ");
+        d1.processCode("6666");
+        System.out.println("Invalid Unlock code: ");
+        d1.processCode("7777");
+        System.out.println("Invalid Unlock code: ");
+        d1.processCode("1111");
+        System.out.println("Valid Unlock code: ");
+        d1.processCode("3333");
 
-        d1.processCode("1111"); //opens
-        d1.processCode("2222"); //opens and fires alarm
-        d1.processCode("1234"); //first trial
-        d1.processCode("4321"); //second trial
-        d1.processCode("5555"); //third trial, gets locked
-        d1.processCode("6666"); //invalid unlock code
-        d1.processCode("7777"); //invalid unlock code
-        d1.processCode("1111"); //invalid unlock code
-        d1.processCode("3333"); //valid unlock code, now can be opened or fire alarm
-
-        //change behaviour of door d1 at run time
+        System.out.println("Change behaviour of door d1: Log - Open");
         d1.setCodeHandler(chain2);
+        System.out.println("Open code: ");
+        d1.processCode("1111");
+        System.out.println("1st trial, wrong code: ");
+        d1.processCode("2222"); //door can't handle FireAlarm
+        System.out.println("2nd trial, wrong code: ");
+        d1.processCode("1234");
+        System.out.println("3th trial, wrong code: ");
+        d1.processCode("4321");
+        System.out.println("4th trial, wrong code: ");
+        d1.processCode("3333");
+        System.out.println("Door can't handle Lock");
+
+        System.out.println("Change behaviour of door d1: Log - FireAlarm - Open");
+        d1.setCodeHandler(chain3);
+        System.out.println("Open code: ");
+        d1.processCode("1111");
+        System.out.println("Open and fire the alarm: ");
+        d1.processCode("2222");
+        System.out.println("1st trial, wrong code: ");
+        d1.processCode("1234");
+        System.out.println("2nd trial, wrong code: ");
+        d1.processCode("4321");
+        System.out.println("3th trial, wrong code: ");
+        d1.processCode("5555");
+        System.out.println("4th trial, wrong code: ");
+        d1.processCode("3333"); //invalid unlock code
+        System.out.println("Door can't handle Lock");
+
+        /*
         BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
             System.out.print("Input code: ");
             String inputCode = stdin.readLine();
             d1.processCode(inputCode);
         }
+         */
+
     }
 }
