ad10d2cec4739f85640472ebe14f2423442b72a5
NIU1671253 <116256008+NIU1671253@users.noreply.github.com>
2024-10-14 17:19:23 +0200

    Update Log.java

--- src/Log.java
+++ src/Log.java
@@ -1,3 +1,17 @@
 public class Log extends CodeHandler {
+    public Log(CodeHandler nextHandler) { super(nextHandler); }
 
+    @Override
+    public void handleCode(String code, Door door) {
+        System.out.println("Code: " + code);
+        System.out.println("Door id: " + door.getId());
+        if(door.isLocked())
+            System.out.println("Door state: Locked");
+        else
+            System.out.println("Door state: Unolocked");
+
+        //Imprimir plus date and time
+
+        super.handleCode(code, door);
+    }
 }
