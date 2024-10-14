7bdc27490810bccdfe85826a0d2a6f8caf37367d
Joan <115805319+NIU1667454@users.noreply.github.com>
2024-10-14 20:08:57 +0200

    Add files via upload

---
+++ src/baseNoStates/States.java
@@ -0,0 +1,9 @@
+package baseNoStates;
+
+public class States {
+  //possible states of Doors
+  public static final String LOCKED = "locked";
+  public static final String UNLOCKED = "unlocked";
+  public static final String UNLOCKED_SHORTLY = "unlocked_shortly";
+  public static final String PROPPED = "propped";
+}
