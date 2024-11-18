1bac79ac7c609174753ea6a829442765e4c6a69b
Joan <115805319+NIU1667454@users.noreply.github.com>
2024-11-18 18:27:03 +0100

    Delete src/baseNoStates directory

--- src/baseNoStates/States.java
+++
@@ -1,9 +0,0 @@
-package baseNoStates;
-
-public class States {
-  //possible states of Doors
-  public static final String LOCKED = "locked";
-  public static final String UNLOCKED = "unlocked";
-  public static final String UNLOCKED_SHORTLY = "unlocked_shortly";
-  public static final String PROPPED = "propped";
-}
